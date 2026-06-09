1db87464ca6d80372607d910a9c57be8289cceaa
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-09 11:01:35 -0400

    usuario y eureka

--- ms-eureka/src/main/java/ms_eureka_server/EurekaApplication.java
+++ ms-eureka/src/main/java/ms_eureka_server/EurekaApplication.java
@@ -2,8 +2,10 @@ package ms_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
